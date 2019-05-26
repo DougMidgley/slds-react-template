39cc62f218bebcaa9a41444a0ca94fc942da70ea
Douglas Midgley <douglas.midgley@accenture.com>
2019-05-26 14:47:21 +0200

    heroku has dynamic port assignment

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,7 @@
 
 
 const mode = process.env.NODE_ENV;
+const port = process.env.PORT || 3000;
 console.log("production", mode);
 
 const fastify = require('fastify')({ logger: mode != "production" });
@@ -40,7 +41,7 @@ fastify.route({
 
 const start = async () => {
   try {
-    await fastify.listen(3000);
+    await fastify.listen(port);
     fastify.log.info(`server listening on ${fastify.server.address().port}`);
   } catch (err) {
     fastify.log.error(err);
