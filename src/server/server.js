dafc5ee795bda09ed59d85d664f6b945a80915f9
Douglas Midgley <douglas.midgley@accenture.com>
2019-05-26 14:50:44 +0200

    Additional changes for fastify on heroku

--- src/server/server.js
+++ src/server/server.js
@@ -41,7 +41,7 @@ fastify.route({
 
 const start = async () => {
   try {
-    await fastify.listen(port);
+    await fastify.listen(port, '0.0.0.0');
     fastify.log.info(`server listening on ${fastify.server.address().port}`);
   } catch (err) {
     fastify.log.error(err);
